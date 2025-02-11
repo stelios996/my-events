5e7c92244d13ee58c438b255a187a57c181bd951
Stelios Syrtagias <steliosyrtagias@gmail.com>
2025-02-11 21:23:55 +0200

    custom hook useFetch to fetch initial data from backend

---
+++ frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,33 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = (url, options={}) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        setLoading(true);
+        setError(null);
+
+        const response = await fetch(url, options);
+        if(!response.ok)
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+
+        const result = await response.json();
+        setData(result);
+      }catch(err){
+        setError(err.message);
+      }finally{
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [data, loading, error];
+}
+
+export default useFetch
