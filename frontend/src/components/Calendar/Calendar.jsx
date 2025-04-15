4100d78839f9fc12878e7a7bc8b9a8f7d93acf79
Stelios Syrtagias <steliosyrtagias@gmail.com>
2025-04-15 13:08:36 +0300

    style calendar component

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,11 +1,13 @@
 import React, {useMemo, useState} from 'react';
 import styles from './Calendar.module.css';
 import { addMonths, eachDayOfInterval, endOfMonth, endOfWeek, format, getDay, startOfMonth, startOfWeek, subMonths} from 'date-fns';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCaretLeft, faCaretRight} from "@fortawesome/free-solid-svg-icons";
 
 const Calendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
-  const dayNames = useMemo(() =>{
+  const dayNames = useMemo(() => {
     const weekStart = startOfWeek(new Date(), { weekStartsOn: 0 });
     const weekEnd = endOfWeek(new Date(), { weekStartsOn: 0 });
     const week = eachDayOfInterval({start: weekStart, end: weekEnd});
@@ -13,34 +15,50 @@ const Calendar = () => {
     return week.map(day=>format(day, 'iii'));
   }, []);
 
-  const start = startOfMonth(currentDate);
-  const end = endOfMonth(currentDate);
-  const days = eachDayOfInterval({start, end});
+  const month = useMemo(() => {
+    const start = startOfMonth(currentDate);
+    const end = endOfMonth(currentDate);
+    const days = eachDayOfInterval({start, end});
 
-  const month = [
-    ...new Array(getDay(start)).fill(null),
-    ...days,
-    ...new Array(6 - getDay(end)).fill(null)
-  ];
+    return [
+      ...new Array(getDay(start)).fill(null),
+      ...days,
+      ...new Array(6 - getDay(end)).fill(null)
+    ];
+  }, [currentDate]);
 
   return (
-    <div className={styles.calendarContainer}>
-      <div className={styles.calendarTopContainer}>
-        <button className={styles.navButton} onClick={() => setCurrentDate(subMonths(currentDate, 1))}>{`< ${format(subMonths(currentDate, 1), "MMM")}`}</button>
-        <h2>{format(currentDate, "MMMM yyyy")}</h2>
-        <button className={styles.navButton} onClick={() => setCurrentDate(addMonths(currentDate, 1))}>{`${format(addMonths(currentDate, 1), "MMM")} >`}</button>
-      </div>
+    <>
+      <div className={styles.backgroundCalendar}></div>
+      <div className={styles.calendarContainer}>
 
-      <div className={styles.calendarGrid}>
-        {dayNames.map(weekday => {
-          return <div key={weekday} className={styles.weekday}>{weekday}</div>;
-        })}
-        {month.map( (day, index) => {
-          return day ? <div key={day} className={styles.day}>{format(day, 'dd')}</div> : <div key={index} className={styles.day}></div>;
-        })}
-      </div>
+        <div className={styles.calendarTopContainer}>
+          <button className={styles.navButton} onClick={() => setCurrentDate(subMonths(currentDate, 1))}>
+            <FontAwesomeIcon icon={faCaretLeft} />
+            {`${format(subMonths(currentDate, 1), "MMM")}`}
+          </button>
+          <h2>{format(currentDate, "MMMM yyyy")}</h2>
+          <button className={styles.navButton} onClick={() => setCurrentDate(addMonths(currentDate, 1))}>
+            {`${format(addMonths(currentDate, 1), "MMM")}`}
+            <FontAwesomeIcon icon={faCaretRight} />
+          </button>
+        </div>
 
-    </div>
+        <ul className={styles.calendarGrid}>
+          {dayNames.map(weekday => {
+            return <li key={weekday} className={styles.weekday}>{weekday}</li>;
+          })}
+          {month.map((day, index) => {
+            const isToday = day && format(day, 'MM-dd-yyyy') === format(new Date(), 'MM-dd-yyyy');
+
+            return <li key={day?.toISOString() ?? index} className={`${styles.day} ${isToday ? styles.today : ''}`}>
+                      {day ? format(day, 'dd') : ''}
+                  </li>;
+          })}
+        </ul>
+
+      </div>
+    </>
   );
 }
 
